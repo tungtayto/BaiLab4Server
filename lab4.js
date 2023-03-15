8388e0855bb21e21536b7992901e2bb6bb2c893d
tungdeptrai <tungktph27675@fpt.edu.vn>
2023-03-15 09:52:18 +0700

    first commit

--- lab4.js
+++ lab4.js
@@ -7,7 +7,7 @@ http.createServer(function (req, res) {
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
       var oldpath = files.filetoupload.filepath;
-      var newpath = 'C:/Users/Your Name/' + files.filetoupload.originalFilename;
+      var newpath = 'C:/luufile/' + files.filetoupload.originalFilename;
       fs.rename(oldpath, newpath, function (err) {
         if (err) throw err;
         res.write('File uploaded and moved!');
